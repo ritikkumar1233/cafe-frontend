d0ec5619b34aebfe63f1e27a64f984dae7f20f97
ritikkumar1233 <ritikroushan96905@gmail.com>
2025-07-15 14:34:12 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import { useState } from "react";
-
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Register from "./components/Register";
-import Product from "./components/Product";
-import Products from "./components/Products";
-import Login from "./components/Login";
-import Cart from "./components/Cart";
-import Order from "./components/Order";
-import Admin from "./components/Admin";
-import Orders from "./components/Orders";
-import Footer from "./components/Footer";
-import Users from "./components/Users";
-import Header from "./components/Header";
-import "./App.css";
+import Register from "./components/Register.jsx";
+import Product from "./components/Product.jsx";
+import Products from "./components/Products.jsx";
+import Login from "./components/Login.jsx";
+import Cart from "./components/Cart.jsx";
+import Order from "./components/Order.jsx";
+import Admin from "./components/Admin.jsx";
+import Orders from "./components/Orders.jsx";
+import Footer from "./components/Footer.jsx";
+import Users from "./components/Users.jsx";
+import Header from "./components/Header.jsx";
 function App() {
   return (
     <div>
