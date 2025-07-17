b147a0b59160f1468c1f912938c6dea607ced676
ritikkumar1233 <ritikroushan96905@gmail.com>
2025-07-17 15:13:30 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Register from "./components/Register.jsx";
 import Product from "./components/Product.jsx";
